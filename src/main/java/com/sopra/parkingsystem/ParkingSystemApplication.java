480f257f62c94d5d1953488642ec1d139e005cb5
UllernJ <Ulrik.johannesen@gmail.com>
2024-01-27 23:29:52 +0100

    backend init

---
+++ src/main/java/com/sopra/parkingsystem/ParkingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sopra.parkingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,66 @@
+-- User Roles
+CREATE TABLE IF NOT EXISTS Roles
+(
+    id   serial PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+-- Parking Status
+CREATE TABLE IF NOT EXISTS ParkingStatus
+(
+    id   serial PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+-- Users
+CREATE TABLE IF NOT EXISTS Users
+(
+    id                         serial PRIMARY KEY,
+    name                       VARCHAR(100) NOT NULL,
+    email                      VARCHAR(255) NOT NULL,
+    roleID                     INT          NOT NULL,
+    FOREIGN KEY (roleID) REFERENCES Roles (id),
+    totalParkingSpots          INT          NOT NULL,
+    totalParkingSpotsAvailable INT          NOT NULL
+);
+
+-- Parking Spots
+CREATE TABLE IF NOT EXISTS ParkingSpots
+(
+    id              serial PRIMARY KEY,
+    userID          INT          NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users (id),
+    regNumber       VARCHAR(100) NOT NULL,
+    fromTime        TIMESTAMP    NOT NULL,
+    toTime          TIMESTAMP    NOT NULL,
+    parkingStatusId INT          NOT NULL,
+    FOREIGN KEY (parkingStatusId) REFERENCES ParkingStatus (id)
+);
+
+
+-- Inserting Roles if not exists
+INSERT INTO Roles (name)
+SELECT 'ADMIN'
+WHERE NOT EXISTS(SELECT 1 FROM Roles WHERE name = 'Admin');
+
+INSERT INTO Roles (name)
+SELECT 'USER'
+WHERE NOT EXISTS(SELECT 1 FROM Roles WHERE name = 'USER');
+
+INSERT INTO Roles (name)
+SELECT 'INSPECTOR'
+WHERE NOT EXISTS(SELECT 1 FROM Roles WHERE name = 'INSPECTOR');
+
+-- Inserting Parking Status if not exists
+INSERT INTO ParkingStatus (name)
+SELECT 'PARKED'
+WHERE NOT EXISTS(SELECT 1 FROM ParkingStatus WHERE name = 'PARKED');
+
+INSERT INTO ParkingStatus (name)
+SELECT 'UNPARKED'
+WHERE NOT EXISTS(SELECT 1 FROM ParkingStatus WHERE name = 'UNPARKED');
+
+-- Inserting test Admin if not exists
+INSERT INTO Users (name, email, roleID, totalParkingSpots, totalParkingSpotsAvailable)
+SELECT 'Admin', 'test@test.test', (SELECT id FROM Roles WHERE name = 'ADMIN'), 10, 10
+WHERE NOT EXISTS(SELECT 1 FROM Users WHERE email = 'test@test.test');
