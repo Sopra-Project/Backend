a9439cded86ff381a67c7dd056cb05762204220a
UllernJ <Ulrik.johannesen@gmail.com>
2024-02-21 18:51:16 +0100

    AuthService - environment variables

--- src/main/java/com/sopra/parkingsystem/service/AuthService.java
+++ src/main/java/com/sopra/parkingsystem/service/AuthService.java
@@ -3,6 +3,7 @@ package com.sopra.parkingsystem.service;
 import com.sopra.parkingsystem.model.User;
 import com.sopra.parkingsystem.model.UserCode;
 import com.sopra.parkingsystem.model.dto.CodeAuthDTO;
+import com.sopra.parkingsystem.utils.EnvironmentComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,18 +16,25 @@ public class AuthService {
     private final TokenService tokenService;
     private final MailSenderService mailSenderService;
     private final UserCodeService userCodeService;
+    private final EnvironmentComponent enviromentComponent;
 
     @Autowired
     public AuthService(UserService userService, TokenService tokenService,
-                       MailSenderService mailSenderService, UserCodeService userCodeService) {
+                       MailSenderService mailSenderService, UserCodeService userCodeService,
+                       EnvironmentComponent enviromentComponent) {
         this.userService = userService;
         this.tokenService = tokenService;
         this.mailSenderService = mailSenderService;
         this.userCodeService = userCodeService;
+        this.enviromentComponent = enviromentComponent;
     }
 
     public String login(String email) {
         User user = userService.getUserByEmail(email);
+        if (enviromentComponent.isDev()) {
+            return tokenService.encodeToken(user);
+        }
+
         if (user != null) {
             mailSenderService.sendEmail(user);
             return "Code sent to email";
