5afad51a396e0b090d98255ce9d3e830f7004ea2
UllernJ <Ulrik.johannesen@gmail.com>
2024-04-09 19:09:28 +0200

    Revert "Auth fix"
    This reverts commit 7b1ae4fbb35f4b1cc1298eb9f22f69f45f1e8e0e.

--- src/main/java/com/sopra/parkingsystem/repository/UserCodeRepository.java
+++ src/main/java/com/sopra/parkingsystem/repository/UserCodeRepository.java
@@ -4,9 +4,7 @@ import com.sopra.parkingsystem.model.UserCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserCodeRepository extends JpaRepository<UserCode, Integer> {
-    List<UserCode> findByCode(String code);
+    UserCode findByCode(String code);
 }
